f78d6206f57874d9d87557a84d6daa1b761c604b
Kathegnosis <g8keren@gmail.com>
2022-05-27 17:20:36 -0700

    Update MaxHeap.java

--- src/MaxHeap.java
+++ src/MaxHeap.java
@@ -11,19 +11,108 @@ Comparator comparator;
     public MaxHeap(int capacity, Comparator comparator){
         // Constructor for the max heap
        
+		this.entries = new ArrayList<HeapEntry<K,V>>();
+		this.capacity = capacity;
+		this.comparator = comparator;
+
     }
 
-	public void add(K key, V value){
+	// Defining some helper methods 
 	
+	// Get parent
+	private int getParentIndex(int index) { 
+		return (index - 1) / 2; 
+	}
+ 
+    // Get left child index
+    private int getLeftChildIndex(int index) { 
+		return (2 * index) + 1; 
+	}
+ 
+    // Get right child index
+    private int getRightChildIndex(int index){ 
+		return (2 * index) + 2; 
+	}
+
+	// swapping function (defining it just for aesthetic)
+	private void swap(int index1, int index2) {
+        Collections.swap(entries, index1, index2);
+    }
+
+	// function that will make the tree satisfy max heap property 
+	private void bubbleUpDown(int index) {
+        
+		// checking if its a leaf so i can end early
+		if (index <= heapSize && index > (heapSize / 2) ) {
+            return; 
+		}
+
+		// keys to compare
+		K currentKey = entries.get(index).getKey();
+		K leftKey = entries.get(getLeftChildIndex(index)).getKey();
+		K rightKey = entries.get(getRightChildIndex(index)).getKey();
+		
+		// how compare works:
+		// compare(Object obj1, Object obj2)
+		// if obj1 > obj2, return positive value
+		// if obj1 < obj2, return negative value
+		// if obj1 = obj2, return 0
+
+		// current key compared to the left child key (is left greater than currnet)
+		int leftCurrentCompared = comparator.compare(leftKey, currentKey);
+		boolean leftGreaterThanCurrent = leftCurrentCompared > 1;
+
+		// current key compared to the right child key  (is right greater than current)
+		int rightCurrentCompared = comparator.compare(rightKey, currentKey);
+		boolean rightGreaterThanCurrent = rightCurrentCompared>1;
+
+		// left key compared to right child key  key (is left greater than right)
+		int leftRightCompared = comparator.compare(leftKey, rightKey);
+		boolean leftGreaterThanRight = leftRightCompared > 1;
+
+		// does the current parent need to be swapped?
+        if (leftGreaterThanCurrent|| rightGreaterThanCurrent) {
+
+			// if the left child is greater than the right child,
+			// then I will swap the left with the parent
+            if (leftGreaterThanRight) {
+                swap(index, getLeftChildIndex(index));
+                bubbleUpDown(getLeftChildIndex(index));
+			} 
+			
+			// otherwise, swap the right with the parent 
+			// (which is what should happen if right=left)
+			else {
+                swap(index, getRightChildIndex(index));
+                bubbleUpDown(getRightChildIndex(index));
+            }
+
+        }
+
+    }
+
+
+
+
+	public void add(K key, V value){
 	    // Method to add the key value pair in the heap, remember to satisfy max heap Property
+
+
+
+
+
 	}
 
 	public HeapEntry<K,V> peek() {
 		// Method to return the max element in the heap
+
+
 	}
 	
 	public HeapEntry<K,V> remove() {
 		//Method to remove the max element in the heap, remember to satisfy max heap Property
+
+
 	}
 	
 }
